878ecff8c4b9de684765f66b39e2f1760bf1e3e9
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-07-02 19:00:56 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonserver = require('json-server');
+const cors = require('cors');
+const port=8080
+const path = require('path');
+const server = jsonserver.create();
+const router = jsonserver.router("db.json");
+
+const middleware = jsonserver.defaults();
+
+server.use(cors());
+server.use(middleware);
+server.use(router);
+server.use(jsonserver.bodyParser);
+
+server.listen(8080, async() => {
+    console.log(`listening on ${port}`);
+    
+})
+
+
